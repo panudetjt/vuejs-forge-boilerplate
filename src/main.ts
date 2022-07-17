f76d1ac05e5d023ddb9ea7fc32b3d72428772ea1
Panudmet Tammwongsa <3482285+panudetjt@users.noreply.github.com>
2022-07-18 01:06:20 +0700

    build!: update dependencies
    BREAKING CHANGE: Remove kendo ui and some dependencies.

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "@/assets/base.css";
-import "@progress/kendo-theme-default/dist/all.css";
 
 const app = createApp(App);
 
